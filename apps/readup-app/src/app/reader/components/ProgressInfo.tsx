243dda2ab214d8476c04a6afe581f0b14e5a8b74
danloh <lploh2016@gmail.com>
2025-08-10 20:15:56 -0700

    chore: i18n

--- apps/readup-app/src/app/reader/components/ProgressInfo.tsx
+++ apps/readup-app/src/app/reader/components/ProgressInfo.tsx
@@ -59,13 +59,10 @@ const ProgressInfoView: React.FC<PageInfoProps> = ({
     ? formatReadingProgress(section?.current, section?.total, formatTemplate)
     : formatReadingProgress(pageinfo?.current, pageinfo?.total, formatTemplate);
 
-  const timeLeft = timeinfo
-    ? _('{{time}} min ·', { time: Math.round(timeinfo.section) })
-    : '';
+  const timeLeft = timeinfo ? _('{{time}} min', { time: Math.round(timeinfo.section) }) : '';
   const { page = 0, pages = 0 } = view?.renderer || {};
-  const pageLeft =
-    pages - 1 > page ? _('· {{count}} pages', { count: pages - 1 - page }) : '';
-  const remainingInfo = `${timeLeft}${pageLeft}`;
+  const pageLeft = pages - 1 > page ? _('{{count}} pages', { count: pages - 1 - page }) : '';
+  const remainingInfo = `${timeLeft}${timeLeft && pageLeft ? ' · ' : ''}${pageLeft}`;
 
   return (
     <div
