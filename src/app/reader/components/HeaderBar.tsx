0fdf5377ea03d79c8550da4fd3a49a7ae722c30e
danloh <lploh2016@gmail.com>
2026-05-31 10:31:26 -0700

    fix dict

--- src/app/reader/components/HeaderBar.tsx
+++ src/app/reader/components/HeaderBar.tsx
@@ -246,6 +246,8 @@ const HeaderBar: React.FC<HeaderBarProps> = ({
           <SettingsToggler bookKey={bookKey} />
           <NotebookToggler bookKey={bookKey} />
           <Dropdown
+            label={_('View Options')}
+            containerClassName='h-8'
             className='exclude-title-bar-mousedown dropdown-bottom dropdown-end'
             buttonClassName='btn btn-ghost h-8 min-h-8 w-8 p-0'
             toggleButton={<PiDotsThreeVerticalBold size={iconSize16} />}
